0c260accdfdbfe68a41837aa144cc9161f3f3a66
Peter Zeidler <peterzeidler89@gmail.com>
2019-09-17 17:27:30 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
  
-MOCK_MODULES = ['numpy', 'scipy', 'astropy', 'pyspeckit']
+MOCK_MODULES = ['numpy', 'scipy', 'astropy','astropy.stats','astropy.io', 'pyspeckit']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
