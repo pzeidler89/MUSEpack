6c0ee8bfb15f3a5ee27387bbc2592c5d94512fda
Peter Zeidler <peterzeidler89@gmail.com>
2020-02-04 17:38:55 -0500

    more updates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='MUSEpack',
-      version='1.1.0',
+      version='1.1.0dev',
       packages=['MUSEpack'],
       author = 'Peter Zeidler',
       author_email = 'peterzeidler89@gmail.com',
