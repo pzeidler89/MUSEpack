ee9cb6363d7afccd7a7bfe6aaed1e54f56243ad2
Peter Zeidler <peterzeidler89@gmail.com>
2024-03-18 17:09:43 -0400

    changing paths to os.path.join

--- MUSEpack/MUSEreduce.py
+++ MUSEpack/MUSEreduce.py
@@ -633,7 +633,7 @@ def _sort_data(self):
                 if ESO_calibration_filename in xmlraw_superstring and not\
                         os.path.isfile(os.path.join(ESO_calibration_dir_temp, ESO_calibration_type[ifile])):
 
-                    shutil.copy(os.path.join(self.raw_data_dir, files), os.path.join(ESO_calibration_dir_temp, ESO_calibration_type[ifile] + '.fits'))
+                    shutil.copy(os.path.join(self.raw_data_dir, ESO_calibration_file), os.path.join(ESO_calibration_dir_temp, ESO_calibration_type[ifile] + '.fits'))
 
         for calibfiles in range(len(calibration_files)):
 
