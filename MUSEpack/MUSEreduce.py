51d89c04132f09e09c5bdbb6375c2aca96846b22
Peter Zeidler <peterzeidler89@gmail.com>
2024-03-29 16:24:10 -0400

    changing paths to os.path.join removed option for turning of baryocentric correction, since it is quite useless to turn it off. Can still be set as manual kwargs moving "renew_statics" and "using_ESO_calibration" to global parameters in config file bugfixes

--- MUSEpack/MUSEreduce.py
+++ MUSEpack/MUSEreduce.py
@@ -2022,7 +2022,7 @@ def _dither_collect(self, exp_list_SCI, OB):
 
     if len(self.user_list) > 0:
         unique_pointings = [os.path.join(self.working_dir, self.user_list[0][:18])]
-        print(unique_pointings)
+
     for unique_pointing_num in range(len(unique_pointings)):
         unique_pointings_ID = unique_pointings[unique_pointing_num][-18:]
         sec = unique_pointings[unique_pointing_num]
@@ -2041,7 +2041,6 @@ def _dither_collect(self, exp_list_SCI, OB):
         if not self.dithering_multiple_OBs:
                 combining_exposure_dir_withoutsky = os.path.join(sec, 'withoutsky')
                 combining_exposure_dir_withsky = os.path.join(sec, 'withsky')
-        print(combining_exposure_dir_withoutsky)
 
         if not os.path.exists(combining_exposure_dir_withoutsky):
             os.makedirs(combining_exposure_dir_withoutsky)
@@ -2065,7 +2064,9 @@ def _dither_collect(self, exp_list_SCI, OB):
         if len(self.user_list) == 0:
             exp_list = glob.glob(sec + '*SCI.list')
         if len(self.user_list) > 0:
-            exp_list = os.path.join(self.working_dir, self.user_list + '_SCI.list')
+            exp_list = []
+            for user_list_element in self.user_list:
+                exp_list.append(os.path.join(self.working_dir, user_list_element + '_SCI.list'))
 
         if self.dithering_multiple_OBs:
             combining_exposure_dir_withoutsky = os.path.join(self.combining_OBs_dir, unique_pointings_ID, 'withoutsky')
