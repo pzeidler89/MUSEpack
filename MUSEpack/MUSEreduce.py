bacd65c54d83f3f95786fca913f5f6b9eea952e9
Peter Zeidler <peterzeidler89@gmail.com>
2024-03-29 16:14:49 -0400

    changing paths to os.path.join removed option for turning of baryocentric correction, since it is quite useless to turn it off. Can still be set as manual kwargs moving "renew_statics" and "using_ESO_calibration" to global parameters in config file bugfixes

--- MUSEpack/MUSEreduce.py
+++ MUSEpack/MUSEreduce.py
@@ -2021,7 +2021,7 @@ def _dither_collect(self, exp_list_SCI, OB):
                     unique_tester = unique_tester + exp_list_SCI[expnum][:-16]
 
     if len(self.user_list) > 0:
-        unique_pointings = os.path.join(self.working_dir, np.array([self.user_list[0][:18]], dtype=object))
+        unique_pointings = os.path.join(self.working_dir, self.user_list[0][:18])
 
     for unique_pointing_num in range(len(unique_pointings)):
         unique_pointings_ID = unique_pointings[unique_pointing_num][-18:]
